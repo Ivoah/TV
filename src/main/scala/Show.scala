afb4ebe4cb6de24faf709f4f381e75c9c7c26b6f
Noah Rosamilia <noah@ivoah.net>
2024-04-29 22:57:13 -0400

    Fix last episode calculation

--- src/main/scala/Show.scala
+++ src/main/scala/Show.scala
@@ -10,7 +10,26 @@ case class Show(title: String, episodes: Int, last_watched: (String, Date), watc
 object Show {
   def getAll()(implicit db: Connection): Seq[Show] = {
     Using.resource({
-      db.prepareStatement("SELECT `show` `title`, count(*) `episodes`, max(date) `last_watched`, episode, group_concat(watched_with SEPARATOR ', ') `watched_with` FROM tv GROUP BY `show` ORDER BY `last_watched` DESC;")
+      db.prepareStatement(
+        """SELECT
+          |  t1.`show` `title`,
+          |  t2.`episodes`,
+          |  t1.`date` `last_watched`,
+          |  t1.`episode`,
+          |  t2.`watched_with`
+          |FROM tv t1
+          |RIGHT JOIN (
+          |  SELECT
+          |    max(id) `max_id`,
+          |    `show`,
+          |    count(*) `episodes`,
+          |    group_concat(watched_with SEPARATOR ', ') `watched_with`
+          |  FROM tv
+          |  GROUP BY `show`
+          |) t2
+          |ON t1.id = t2.max_id AND t1.show = t2.show
+          |ORDER BY date DESC
+          |""".stripMargin)
     }) { stmt =>
       val results = stmt.executeQuery()
       val buffer = mutable.Buffer[Show]()
